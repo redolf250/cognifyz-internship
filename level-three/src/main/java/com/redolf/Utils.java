a4cdf759be1a9e20f3ab0cf8c9f822cc4261caf0
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-12-14 19:48:50 +0000

    Create Utils.java

---
+++ level-three/src/main/java/com/redolf/Utils.java
@@ -0,0 +1,6 @@
+package com.redolf;
+
+public class Utils {
+    public static final String API_KEY = "e90528adaa2a012d64f93775";
+    public static final String URL = "https://v6.exchangerate-api.com/v6/%s/latest/%s";
+}
