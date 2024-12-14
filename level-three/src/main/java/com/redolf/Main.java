1d415ae496e68ad0a9242c4be00c9f5844fa212f
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-12-14 19:48:53 +0000

    Update Main.java

--- level-three/src/main/java/com/redolf/Main.java
+++ level-three/src/main/java/com/redolf/Main.java
@@ -1,7 +1,23 @@
 package com.redolf;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        CurrencyConverterServiceImpl service = new CurrencyConverterServiceImpl();
+//        System.out.println(service.getExchangeRate("USD","GHS"));
+//        System.out.println(service.convertCurrency(1000,"USD","GHS"));
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("==============Currency Converter Console Service============");
+
+        System.out.print("Enter the base currency (e.g., USD): ");
+        String baseCurrency = scanner.next().toUpperCase();
+        System.out.print("Enter the target currency (e.g., EUR): ");
+        String targetCurrency = scanner.next().toUpperCase();
+        System.out.print("Enter the amount: ");
+        double amount = scanner.nextDouble();
+        service.convertCurrency(amount, baseCurrency, targetCurrency);
+        scanner.close();
     }
 }
