3cd3010a35190691e50ddf680aa37ad9e3fe2f3b
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-12-14 09:08:35 +0000

    Create PasswordGeneratorServiceImplTest.java

---
+++ level-one/src/test/java/com/redolf/taskFour/PasswordGeneratorServiceImplTest.java
@@ -0,0 +1,30 @@
+package com.redolf.taskFour;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordGeneratorServiceImplTest {
+
+   private PasswordGeneratorServiceImpl service;
+    @BeforeEach
+    void setUp() {
+        service = new PasswordGeneratorServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+        service = null;
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(PasswordArgumentsProvider.class)
+    void generatePassword(int length, boolean number, boolean lowerCase, boolean upperCase, boolean specialChar) {
+        String password = service.generatePassword(length, number, lowerCase, upperCase, specialChar);
+        assertEquals(password.length(), length);
+        assertNotNull(password);
+    }
+}
