8383d169accab7fc8e20680e866c732af4c73729
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-12-14 09:07:50 +0000

    Create Utils.java

---
+++ level-one/src/main/java/com/redolf/taskFour/Utils.java
@@ -0,0 +1,8 @@
+package com.redolf.taskFour;
+
+public class Utils {
+    public static String NUMBER = "0123456789";
+    public static String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    public static String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static String SPECIAL_CHARACTER = "!@#$%^&*()-_=+[]{}|;:',.<>?/";
+}
