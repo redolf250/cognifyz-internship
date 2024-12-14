5fa4933a915ed941a30121768b49a91936b6f97e
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-12-14 09:07:45 +0000

    Create PasswordGeneratorServiceImpl.java

---
+++ level-one/src/main/java/com/redolf/taskFour/PasswordGeneratorServiceImpl.java
@@ -0,0 +1,31 @@
+package com.redolf.taskFour;
+
+import java.util.Random;
+
+public class PasswordGeneratorServiceImpl implements PasswordGeneratorService{
+
+    @Override
+    public String generatePassword(int length, boolean includeNumbers, boolean includeLowercase, boolean includeUppercase, boolean includeSpecialChars) {
+
+        StringBuilder characterPool = new StringBuilder();
+
+        if (includeNumbers) characterPool.append(Utils.NUMBER);
+        if (includeLowercase) characterPool.append(Utils.LOWERCASE);
+        if (includeUppercase) characterPool.append(Utils.UPPERCASE);
+        if (includeSpecialChars) characterPool.append(Utils.SPECIAL_CHARACTER);
+
+        if (characterPool.isEmpty()) {
+            System.out.println("You must select at least one character type!");
+            return "";
+        }
+
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characterPool.length());
+            password.append(characterPool.charAt(index));
+        }
+        return password.toString();
+    }
+}
